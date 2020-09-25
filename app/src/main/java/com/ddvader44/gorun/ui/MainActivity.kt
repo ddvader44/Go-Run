7b5ca9ef6562631221d79825884d5258cf7eb8b3
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-09-25 20:54:51 +0530

    nav

--- app/src/main/java/com/ddvader44/gorun/ui/MainActivity.kt
+++ app/src/main/java/com/ddvader44/gorun/ui/MainActivity.kt
@@ -2,21 +2,33 @@ package com.ddvader44.gorun.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ddvader44.gorun.R
 import com.ddvader44.gorun.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDao: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment , R.id.runFragment , R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 
 }
--- app/src/main/java/com/ddvader44/gorun/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/ddvader44/gorun/ui/fragments/RunFragment.kt
@@ -1,12 +1,22 @@
 package com.ddvader44.gorun.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.ddvader44.gorun.R
 import com.ddvader44.gorun.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run){
-private val viewModel : MainViewModel by viewModels()
+    private val viewModel : MainViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
