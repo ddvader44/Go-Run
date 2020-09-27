e19807dac1217f93410bedaa56daca0cbfc6b197
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-09-28 01:47:01 +0530

    location permissions

--- app/src/main/java/com/ddvader44/gorun/extras/Constants.kt
+++ app/src/main/java/com/ddvader44/gorun/extras/Constants.kt
@@ -2,4 +2,5 @@ package com.ddvader44.gorun.extras
 
 object Constants {
     const val RUNNING_DATABASE_NAME = "running_db"
+    const val REQUEST_CODE_LOCATION_PERMISSION=0
 }
---
+++ app/src/main/java/com/ddvader44/gorun/extras/TrackingUtility.kt
@@ -0,0 +1,28 @@
+package com.ddvader44.gorun.extras
+
+import android.content.Context
+import android.os.Build
+import pub.devrel.easypermissions.EasyPermissions
+import android.Manifest
+
+object TrackingUtility {
+
+    fun hasLocationPermissions(context:Context) =
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else{
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+
+
+}
