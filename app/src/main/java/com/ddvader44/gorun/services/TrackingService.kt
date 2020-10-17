56ac391ea88f95e0c38e3c7877db4954783facc3
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-10-18 03:45:33 +0530

    cancel run

--- app/src/main/java/com/ddvader44/gorun/services/TrackingService.kt
+++ app/src/main/java/com/ddvader44/gorun/services/TrackingService.kt
@@ -50,6 +50,7 @@ typealias Polylines = MutableList<Polyline>
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
+    var serviceKilled = false
 
     @Inject
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -88,6 +89,16 @@ class TrackingService : LifecycleService() {
         timeRunInMillis.postValue(0L)
     }
 
+    private fun killService() {
+        serviceKilled = true
+        isFirstRun = true
+        pauseService()
+        postInitialValues()
+        stopForeground(true)
+        stopSelf()
+    }
+
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when(it.action){
@@ -106,6 +117,7 @@ class TrackingService : LifecycleService() {
                 }
                 ACTION_STOP_SERVICE -> {
                     Timber.d("Stop service")
+                    killService()
                 }
             }
         }
@@ -165,10 +177,13 @@ class TrackingService : LifecycleService() {
             isAccessible = true
             set(curNotificationBuilder, ArrayList<NotificationCompat.Action>())
         }
-        curNotificationBuilder = baseNotificationBuilder
-            .addAction(R.drawable.ic_pause_black_24dp,notificationActionText,pendingIntent)
+        if(!serviceKilled){
+            curNotificationBuilder = baseNotificationBuilder
+                .addAction(R.drawable.ic_pause_black_24dp,notificationActionText,pendingIntent)
 
-        notificationManager.notify(NOTIFICATION_ID,curNotificationBuilder.build())
+            notificationManager.notify(NOTIFICATION_ID,curNotificationBuilder.build())
+        }
+
 
     }
 
@@ -244,9 +259,12 @@ class TrackingService : LifecycleService() {
         startForeground(NOTIFICATION_ID, baseNotificationBuilder.build())
 
         timeRunInSeconds.observe(this, Observer {
-            val notification = curNotificationBuilder
-                .setContentText(TrackingUtility.getFormattedStopWatchTime(it * 1000L))
-            notificationManager.notify(NOTIFICATION_ID,notification.build())
+            if(!serviceKilled){
+                val notification = curNotificationBuilder
+                    .setContentText(TrackingUtility.getFormattedStopWatchTime(it * 1000L))
+                notificationManager.notify(NOTIFICATION_ID,notification.build())
+            }
+
         })
 
     }
--- app/src/main/java/com/ddvader44/gorun/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/ddvader44/gorun/ui/fragments/TrackingFragment.kt
@@ -1,15 +1,18 @@
 package com.ddvader44.gorun.ui.fragments
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.ddvader44.gorun.R
 import com.ddvader44.gorun.extras.Constants
 import com.ddvader44.gorun.extras.Constants.ACTION_PAUSE_SERVICE
 import com.ddvader44.gorun.extras.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.ddvader44.gorun.extras.Constants.ACTION_STOP_SERVICE
 import com.ddvader44.gorun.extras.Constants.MAP_ZOOM
 import com.ddvader44.gorun.extras.Constants.POLYLINE_COLOR
 import com.ddvader44.gorun.extras.Constants.POLYLINE_WIDTH
@@ -20,6 +23,7 @@ import com.ddvader44.gorun.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -33,6 +37,17 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking){
     private var map: GoogleMap? = null
 
     private var currentTimeMillis = 0L
+
+    private var menu: Menu? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -67,10 +82,53 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking){
 
     private fun toggleRun(){
         if(isTracking){
+            menu?.getItem(0)?.isVisible = true
             sendCommandToService(ACTION_PAUSE_SERVICE)
         }else{
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.toolbar_tracking_menu,menu)
+        this.menu = menu
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        if(currentTimeMillis > 0L){
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miCancelTracking -> {
+                showCancelTrackingDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCancelTrackingDialog() {
+        val dialog = MaterialAlertDialogBuilder(requireContext(),R.style.AlertDialogTheme)
+            .setTitle("Cancel Run?")
+            .setMessage("Are you sure you want to cancel the run?")
+            .setIcon(R.drawable.ic_delete)
+            .setPositiveButton("Yes") { _, _ ->
+                stopRun()
+            }
+            .setNegativeButton("No") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+            .create()
+        dialog.show()
+    }
+
+    private fun stopRun() {
+        sendCommandToService(ACTION_STOP_SERVICE)
+        findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
     }
 
     private fun updateTracking(isTracking: Boolean){
@@ -80,6 +138,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking){
             btnFinishRun.visibility = View.VISIBLE
         }else{
             btnToggleRun.text = "Stop"
+            menu?.getItem(0)?.isVisible = true
             btnFinishRun.visibility = View.GONE
         }
     }
